1cbda165638f09d3cb0f3a70f8561c7af1dd2091
dixardev <pauldigitalwriter@gmail.com>
2024-07-16 15:09:13 +0100

    a

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Connect to MongoDB using the connection string from the .env file
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
